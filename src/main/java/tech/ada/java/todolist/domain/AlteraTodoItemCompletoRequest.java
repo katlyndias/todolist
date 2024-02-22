7f92fda920ebd0c13c2b9875f1b7be6d3cc7181d
katlyndias <148639020+katlyndias@users.noreply.github.com>
2024-02-22 07:55:18 -0300

    Utilização da mensagem dentro do Objects.requireNonNull();

--- src/main/java/tech/ada/java/todolist/domain/AlteraTodoItemCompletoRequest.java
+++ src/main/java/tech/ada/java/todolist/domain/AlteraTodoItemCompletoRequest.java
@@ -17,7 +17,7 @@ public class AlteraTodoItemCompletoRequest {
         LocalDate prazoFinal;
     // construtor - reescrevendo o default
     public AlteraTodoItemCompletoRequest(String titulo, String descricao, Boolean concluida, LocalDateTime dataHora, LocalDate prazoFinal){
-        this.titulo = Objects.requireNonNull(titulo); // obrigando a não ser nulo, a partir do Java 16 essa classe Objects
+        this.titulo = Objects.requireNonNull(titulo, "Título é obrigatório"); // obrigando a não ser nulo, a partir do Java 16 essa classe Objects, tem versão sem mensagem e com sobrecarga com mensagem
         this.descricao = Objects.requireNonNull(descricao);
         this.concluida = Objects.requireNonNull(concluida);
         this.dataHora = Objects.requireNonNull(dataHora);
